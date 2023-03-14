fa2e3da10ef4623d7ccadfce1970d3deec6f57dd
m1taylor <m1taylor@ryerson.ca>
2023-03-14 17:36:40 -0400

    user schema

--- models/user.js
+++ models/user.js
@@ -1,2 +1,35 @@
 const { Schema, model,} = require('mongoose');
 const reactionSchema = require('./Reaction');
+
+//
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            //REGEX for email
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
