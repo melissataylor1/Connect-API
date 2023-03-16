73b73b0a271052c025d20c168e68a2d05f02b01f
m1taylor <m1taylor@ryerson.ca>
2023-03-16 17:53:10 -0400

    added data arrays

---
+++ utils/data.js
@@ -0,0 +1,30 @@
+const usernames = [
+    'SixToes',
+    'LeoTaylor',
+    'PumpkinTaylor',
+    'CookingMama',
+    'GamerGuy',
+    'ManExample',
+    'MelTaylor',
+    'DarkChocolate',
+
+  ];
+
+  const thought = [
+    'Love learning web development',
+    'I am a cat meow',
+    'Politics these days...',
+    'Check this new recipe',
+
+  ];
+  
+  
+  const reactions = [
+    'Like!',
+    'Dislike!',
+    'Funny',
+    'Sad',
+    'Heartwarming',
+ 
+  ];
+ 
