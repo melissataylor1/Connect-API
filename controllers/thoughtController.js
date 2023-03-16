a77316a6629b6488a42da0d064ffbd398e53e9aa
m1taylor <m1taylor@ryerson.ca>
2023-03-16 16:09:49 -0400

    get single thoughts by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,5 +8,18 @@ module.exports = {
             .then((thought) => res.json(thought))
             .catch((err) => res.status(500).json(err))
     },
-    },
 
+   // GET single thought by its _id
+   getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+        .then((thoughtId) =>
+            !thoughtId
+                ? res.status(404).json({ message: "No thought found with that id" })
+                : res.json(thoughtId)
+        )
+        .catch((err) => res.status(500).json(err));
+},
+
+
+
+};
