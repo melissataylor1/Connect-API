7463ecd133933eb4698d48da01a80cdcd27a4dd6
m1taylor <m1taylor@ryerson.ca>
2023-03-16 16:45:11 -0400

    fuxed date format

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,8 +1,7 @@
 const { format } = require('date-fns');
 
 function dateFormat(date) {
-    // This formats the date as 'March 14th, 2023 3:30 PM'
-    return format(date, 'MMMM do, yyyy h:mm a');
+    return format(date, 'MMMM do, yyyy - h:mm a');
 }
 
 module.exports = dateFormat;
