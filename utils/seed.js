ca1de7229d46c058844e2c80dc2355fd3a562672
m1taylor <m1taylor@ryerson.ca>
2023-03-16 17:42:35 -0400

    mongo db connect

--- utils/seed.js
+++ utils/seed.js
@@ -7,16 +7,15 @@ const {
   genRandomIndex,
 } = require('./data');
 
-// Start the seeding runtime timer
+// seeding timer
 console.time('seeding');
 
-// Creates a connection to mongodb
+// mongodb connect
 connection.once('open', async () => {
-  // Delete the entries in the collection
   await Thought.deleteMany({});
   await User.deleteMany({});
   const userData =[]
-  // Empty arrays for randomly generated posts and reactions
+  // Empty arrays for randomly generated posts and reactions for insomnia
   const reactions = [...getRandomReaction(10)];
   const thoughts = [];
   for(let i=0; i<10; i++ ){
@@ -27,27 +26,3 @@ connection.once('open', async () => {
       username
     });
   }
-  // Makes reactions array
-  const makeThought = (thoughtText) => {
-    thoughts.push({
-      thoughtText,
-      username: getRandomUserName().split(' ')[0],
-      reactions: [reactions[genRandomIndex(reactions)]._id],
-    });
-  };
-
-  // Wait for the reactions to be inserted into the database
-  await User.collection.insertMany(userData);
-
-  // For each of the reactions that exist, make a random post of 10 words
-  reactions.forEach(() => makeThought(getRandomThought(10)));
-
-  // Wait for the posts array to be inserted into the database
-  await Thought.collection.insertMany(thoughts);
-
-  // Log out a pretty table for reactions and posts
-  console.table(reactions);
-  console.table(thoughts);
-  console.timeEnd('seeding complete 🌱');
-  process.exit(0);
-});
